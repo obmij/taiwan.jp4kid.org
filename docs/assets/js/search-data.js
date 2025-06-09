f260617b69a027d054d676ab824eabb87bf161a0
obmij <82932766+obmij@users.noreply.github.com>
2025-06-09 09:46:24 +0800

    Add files via upload

---
+++ docs/assets/js/search-data.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "救助兒童必要從預防暴力開始",
+        "excerpt":"","categories": ["learn"],
+        "tags": [],
+        "url": "http://localhost:4000/learn/save-children.html"
+      },{
+        "title": "了解關鍵詞彙",
+        "excerpt":"#兒童權利公約；CRC人人有權享有公約所載的一切權利和自由，不分種族、膚色、性別、語言、宗教、政治或其他見解、國籍或社會出身、財產、殘疾、出生或其他身分等任何區別。兒童有權享有特殊照顧和援助。兒童是指未滿十八歲的任何人（除非根據適用於兒童的法律，成年年齡提前）。兒童的最大利益應是首要考慮因素。確保兒童獲得其福祉所需的保護和照顧。 #毒性壓力 ","categories": ["learn"],
+        "tags": [],
+        "url": "http://localhost:4000/learn/Understand-the-terms.html"
+      }]
