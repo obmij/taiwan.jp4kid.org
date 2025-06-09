db1896df3b7476904a601e2decffd5fdd9d9cb5b
obmij <82932766+obmij@users.noreply.github.com>
2025-06-09 09:45:08 +0800

    Add files via upload

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,11 +1,43 @@
-var store = [{
-        "title": "救助兒童必要從預防暴力開始",
-        "excerpt":"","categories": ["learn"],
-        "tags": [],
-        "url": "http://localhost:4000/learn/save-children.html"
-      },{
-        "title": "了解關鍵詞彙",
-        "excerpt":"#兒童權利公約；CRC人人有權享有公約所載的一切權利和自由，不分種族、膚色、性別、語言、宗教、政治或其他見解、國籍或社會出身、財產、殘疾、出生或其他身分等任何區別。兒童有權享有特殊照顧和援助。兒童是指未滿十八歲的任何人（除非根據適用於兒童的法律，成年年齡提前）。兒童的最大利益應是首要考慮因素。確保兒童獲得其福祉所需的保護和照顧。 #毒性壓力 ","categories": ["learn"],
-        "tags": [],
-        "url": "http://localhost:4000/learn/Understand-the-terms.html"
-      }]
+---
+layout: null
+sitemap: false
+---
+
+var store = [
+  {%- for c in site.collections -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+    {%- assign docs = c.docs | where_exp: 'doc', 'doc.search != false' -%}
+    {%- for doc in docs -%}
+      {
+        "title": {{ doc.title | jsonify }},
+        "excerpt":
+          {%- if site.search_full_content == true -%}
+          {{ doc.content | 
+            replace:"</p>", " " | 
+            replace:"</h1>", " " | 
+            replace:"</h2>", " " | 
+            replace:"</h3>", " " | 
+            replace:"</h4>", " " | 
+            replace:"</h5>", " " | 
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | jsonify }},
+        {%- else -%}
+          {{ doc.content | 
+            replace:"</p>", " " | 
+            replace:"</h1>", " " | 
+            replace:"</h2>", " " | 
+            replace:"</h3>", " " | 
+            replace:"</h4>", " " | 
+            replace:"</h5>", " " | 
+            replace:"</h6>", " "|
+           strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+        {%- endif -%}
+        "categories": {{ doc.categories | jsonify }},
+        "tags": {{ doc.tags | jsonify }},
+        "url": {{ doc.url | absolute_url | jsonify }}
+      } {%- unless forloop.last and l -%}, {%- endunless -%}
+    {%- endfor -%}
+  {%- endfor -%}
+]
